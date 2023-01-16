f580646710de7511bc2dd761d64e11008d998b63
yoandref <andredepaulacontato@gmail.com>
2023-01-15 21:03:01 -0300

    Primeiro commit

---
+++ src/test/java/io/github/yoandreferreira/shopapi/shopapi/ShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.yoandreferreira.shopapi.shopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
